0402c5929e24daa8521ac1d0d3b2e297b3bf102d
muznahrehman21 <mail2m.rehman@gmail.com>
2023-12-03 02:08:24 +0500

    merge

---
+++ lib/views/tabs/notification_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+class NotificationScreen extends StatefulWidget {
+  const NotificationScreen({Key? key}) : super(key: key);
+
+  @override
+  State<NotificationScreen> createState() => _NotificationScreenState();
+}
+
+class _NotificationScreenState extends State<NotificationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notifications'),
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: Icon(Icons.notifications),
+            title: Text('New Notification'),
+            subtitle: Text('Description of notification #$index'),
+            trailing: Icon(Icons.arrow_forward_ios),
+            onTap: () {
+
+            },
+          );
+        },
+      ),
+    );
+  }
+}
